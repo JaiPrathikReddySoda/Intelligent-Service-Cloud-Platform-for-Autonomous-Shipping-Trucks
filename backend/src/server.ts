2e943feaecbc9a7dd7e9c0752d9bf11cb617140a
JaiPrathikReddySoda <jaiprathik.reddys@gmail.com>
2025-05-21 15:26:27 -0700

    Add files via upload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import trucksRouter from './routes/trucks';
+import serviceRequestRoutes from './routes/serviceRequest';
+import scheduleRoutes from './routes/schedule';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/trucks', trucksRouter);
+app.use('/api/servicerequests', serviceRequestRoutes);
+app.use('/api/schedule', scheduleRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI || '', {}).then(() => {
+  console.log('✅ MongoDB connected');
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
+}).catch((err) => {
+  console.error('❌ MongoDB connection failed:', err);
+});
 
